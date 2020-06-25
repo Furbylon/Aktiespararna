408287bc17f6deba3b257f8de243c88b6179d905
Davidfurby <57434673+Davidfurby@users.noreply.github.com>
2020-06-25 13:11:58 +0200

    Refactored the portfolio pageSwitch

--- aktiespararna/src/MainMenu/Min Portfolio/Portfolio.js
+++ aktiespararna/src/MainMenu/Min Portfolio/Portfolio.js
@@ -1,13 +1,14 @@
 import React from 'react';  
 import Header from "../Components/Header"
-import Portfolioframe from './PortfolioComponenter.js/Portfolioframe'
+import Portfolioframe from "../Min Portfolio/Portfolio Components/Portfolioframe"
 import './Portfolio css/Portfolio.css'
-
+import LastUpdate from "../Components/LastUpdate"
 
 const Portfolio = () => {
     return (
       <div id = "portfolio">
       <Header head = {"Portfolio"}/>
+      <LastUpdate/>
       <Portfolioframe/>
       </div>
     );
