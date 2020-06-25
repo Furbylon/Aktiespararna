7ae9f8901247ab36ba81b3ca15a4ddd0a4703b26
Davidfurby <57434673+Davidfurby@users.noreply.github.com>
2020-06-25 17:40:24 +0200

    Work on Pagination

--- aktiespararna/src/MainMenu/Min Portfolio/Portfolio Components/PageSwitch.js
+++ aktiespararna/src/MainMenu/Min Portfolio/Portfolio Components/PageSwitch.js
@@ -1,7 +1,8 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 
 const PageSwitch = (props) => {
-    let [shownButtons, setShownButtons]= useState(5)
+  let [shownButtons, setShownButtons] = useState(5);
+
   let SetButtons = () => {
     let arr = [];
     for (let i = 1; i <= shownButtons; i++) {
@@ -9,24 +10,54 @@ const PageSwitch = (props) => {
     }
     let buttons = [];
     buttons = arr.map((value) => (
-      <button value={value * 10} onClick={UpdatePage} id="pageButton" key={value}>
+      <button
+        value={value * 10}
+        onClick={UpdatePage}
+        id="pageButton"
+        key={value}
+      >
         {value}
       </button>
     ));
+
     return buttons;
   };
   let UpdatePage = (e) => {
-      console.log(e.target.value)
-    props.setCurrentPage(e.target.value);
-    props.setCurrentData(props.data.slice(e.target.value - 10, e.target.value));
+    if (e.target.value === "back") {
+      props.setCurrentPage(props.currentPage - 10);
+      props.setCurrentData(props.currentPage - 10, props.currentPage);
+    }
+    if (e.target.value === "forward") {
+      props.setCurrentPage();
+      props.setCurrentData();
+    } else {
+      props.setCurrentPage(e.target.value);
+      props.setCurrentData(
+        props.data.slice(e.target.value - 10, e.target.value)
+      );
+    }
   };
-
-  return (
-    <div style={{position: "absolute", right: "160px"}}>
-      <form>
-        <SetButtons />
-      </form>
-    </div>
-  );
+  if (props.totalData === shownButtons) {
+    return (
+      <div style={{ position: "absolute", right: "160px" }}>
+        <form onSubmit={UpdatePage}>
+          <button value={"back"}>Left</button>
+          <SetButtons />
+          <button value={"forward"}>Right</button>
+        </form>
+      </div>
+    );
+  } else {
+    return (
+      <div style={{ position: "absolute", right: "160px" }}>
+        <form onSubmit={UpdatePage}>
+          <button value={"back"}>Left</button>
+          <SetButtons />
+          <button>...</button>
+          <button value={"forward"}>Right</button>
+        </form>
+      </div>
+    );
+  }
 };
 export default PageSwitch;
