00d28f516b783157819222b418689db31c5393dd
JoakimDenlov <Joakimdenlov1@gmail.com>
2020-06-28 20:44:00 +0200

    Refactor - removed unused vars

--- aktiespararna/src/LoginMenu/ForgottenPassword/ForgottenPassword.js
+++ aktiespararna/src/LoginMenu/ForgottenPassword/ForgottenPassword.js
@@ -1,16 +1,14 @@
 import React from "react";
 import aktielogoo from "../Login/img/aktielogoo.png";
-import userData from "../../data/JSON/user.json";
-import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
 import "./ForgottenPassword.css";
 
 const ForgottenPassword = (props) => {
   const checkPassword = (e) => {
-    console.log(e.target.elements.email.value)
-    console.log(props.email)
-    e.preventDefault()
-    if ((e.target.elements.email.value) === (props.email)) {
+    console.log(e.target.elements.email.value);
+    console.log(props.email);
+    e.preventDefault();
+    if (e.target.elements.email.value === props.email) {
       alert("Your Password is: " + props.password);
     } else {
       alert("No username Found");
@@ -29,7 +27,7 @@ const ForgottenPassword = (props) => {
         <input type="text" id="email" placeholder="Email" required />
         <br></br>
         <div id="button">
-        <button>Hämta lösenord</button>
+          <button>Hämta lösenord</button>
         </div>
         <br></br>
       </form>
--- aktiespararna/src/Main.js
+++ aktiespararna/src/Main.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import Login from "./LoginMenu/Login/Login";
 import Registrering from "./LoginMenu/Register/Register";
