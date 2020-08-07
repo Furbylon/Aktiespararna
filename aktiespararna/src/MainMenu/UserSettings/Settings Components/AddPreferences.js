20a6b82f3166ff8ff3967df6b36cc4eb18a0829a
Davidfurby <57434673+Davidfurby@users.noreply.github.com>
2020-08-07 19:37:04 +0200

    Created file for the addPrefernces-function

---
+++ aktiespararna/src/MainMenu/UserSettings/Settings Components/AddPreferences.js
@@ -0,0 +1,44 @@
+import mock from "../../../data/JSON/mock.json";
+let addPreferences = (
+  e,
+  setPreferredIndustries,
+  setRemainingIndustries,
+  setTime
+) => {
+  e.preventDefault();
+  let checkbox = document.querySelectorAll('input[name="checkbox"]:checked');
+  let checkedIndustries = [];
+  checkbox.forEach((checkbox) => {
+    checkedIndustries.push(checkbox.value);
+  });
+  if (checkedIndustries.length <= 4 && checkedIndustries.length > 0) {
+    let check = [];
+    check = mock.filter((values) => {
+      return checkedIndustries.includes(values.Industry);
+    });
+
+    const uniqueIndustry = Array.from(
+      new Set(check.map((a) => a.Industry))
+    ).map((Industry) => {
+      return check.find((a) => a.Industry === Industry);
+    });
+
+    const remaining = mock.filter((values) => {
+      return !checkedIndustries.includes(values.Industry);
+    });
+
+    setPreferredIndustries(uniqueIndustry);
+    setRemainingIndustries(remaining);
+    let newDate = new Date();
+    setTime(newDate.toLocaleDateString());
+  } else {
+    let newLine = "\r\n";
+    let msg = "- Måste anges minst 1 industri";
+    msg += newLine;
+    msg += "- Kan endast ange max 4 industrier";
+
+    return alert(msg);
+  }
+};
+
+export default addPreferences;
--- aktiespararna/src/MainMenu/UserSettings/Settings Components/Preferences.js
+++ aktiespararna/src/MainMenu/UserSettings/Settings Components/Preferences.js
@@ -3,6 +3,7 @@ import mock from "../../../data/JSON/mock.json";
 import ShowMore from "./ShowMoreIndustriesButton";
 import currentlyShown from "./CurrentlyShownIndustries";
 import ImplementBankButton from "./ImplementBank";
+import addPreferences from "./AddPreferences";
 
 const Preferences = (props) => {
   let IndustryArr = mock.map((value) => {
@@ -13,46 +14,18 @@ const Preferences = (props) => {
   let [max, setMax] = useState(5);
   let [industries, setIndustries] = useState(uniqueIndustryArr.slice(0, max));
 
-  let addPreferences = (e) => {
-    e.preventDefault();
-    let checkbox = document.querySelectorAll('input[name="checkbox"]:checked');
-    let checkedIndustries = [];
-    checkbox.forEach((checkbox) => {
-      checkedIndustries.push(checkbox.value);
-    });
-    if (checkedIndustries.length <= 4 && checkedIndustries.length > 0) {
-      let check = [];
-      check = mock.filter((values) => {
-        return checkedIndustries.includes(values.Industry);
-      });
-
-      const uniqueIndustry = Array.from(
-        new Set(check.map((a) => a.Industry))
-      ).map((Industry) => {
-        return check.find((a) => a.Industry === Industry);
-      });
-
-      const remaining = mock.filter((values) => {
-        return !checkedIndustries.includes(values.Industry);
-      });
-
-      props.setPreferredIndustries(uniqueIndustry);
-      props.setRemainingIndustries(remaining);
-      let newDate = new Date();
-      props.setTime(newDate.toLocaleDateString());
-    } else {
-      let newLine = "\r\n";
-      let msg = "- Måste anges minst 1 industri";
-      msg += newLine;
-      msg += "- Kan endast ange max 4 industrier";
-
-      return alert(msg);
-    }
-  };
-
   return (
     <div>
-      <form onSubmit={addPreferences}>
+      <form
+        onSubmit={(e) => {
+          addPreferences(
+            e,
+            props.setPreferredIndustries,
+            props.setRemainingIndustries,
+            props.setTime
+          );
+        }}
+      >
         <p>Mina prefererade industrier att investera inom:</p>
         <ShowMore
           setMax={setMax}
